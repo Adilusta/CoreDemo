6ac7ef137e263dcd5ef3027bb3e9d5fbf8497217
Adil Usta <klambera@gmail.com>
2023-05-06 15:12:25 +0300

    Bir api projesi oluşturuldu.Örnek bir employee sınıfı ile beraber api üzerinden get,delete,update  işlemleri yapıldı, test edildi.

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,85 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+
+            var values = context.Employees.ToList();
+
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            context.Add(employee);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+            var result = context.Employees.FirstOrDefault(x => x.ID == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(result);
+            }
+   
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var result = context.Employees.FirstOrDefault(x => x.ID == id);
+
+            if(result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                context.Remove(result);
+                context.SaveChanges();
+                return Ok();
+            }
+          
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployee(Employee employee)
+        {
+            var result = context.Employees.FirstOrDefault(x => x.ID == employee.ID);
+            //var deneme = context.Find<Employee>(employee.ID);  bu da oluyor.
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                result.Name = employee.Name;
+                context.Update(result);
+                context.SaveChanges();
+                return Ok(result);
+            }
+
+        }
+    }
+}
---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-5CVFMQ5\\SQLEXPRESS;database=CoreBlogApiDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}

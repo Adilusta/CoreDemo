a68a0d2861fb57d0a413b4f9ca148a7ec06ce03b
Adil Usta <klambera@gmail.com>
2023-07-21 02:47:44 +0300

    Admin panelindeki mesajlar tarafı düzenlendi.Gelen kutusu,giden kutusu ve mesaj oluştur sayfalarının tasarımları eklendi.

---
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        Message2Manager message2Manager = new Message2Manager(new EfMessage2Repository());
+        Context context = new Context();
+        public IActionResult InBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerID = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            var values = message2Manager.GetInboxListByWriter(writerID);
+            return View(values);
+
+        }
+
+        public IActionResult SendBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerID = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            var values = message2Manager.GetSendBoxWithByWriter(writerID);
+            return View(values);
+        }
+
+        public IActionResult ComposeMessage()
+        {
+            return View();
+        }
+    }
+}
